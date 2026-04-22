53ca25c5ca54e7560a8b0a78e92109a50456e10b
Lucas Rocchetti <lucasrocchetti@outlook.com>
2026-04-22 01:33:18 -0400

    Declare canonical host in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,5 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
     },
     sitemap: "https://lucasrocchetti.com/sitemap.xml",
+    host: "https://lucasrocchetti.com",
   };
 }
